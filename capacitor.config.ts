be2f00e9fbbab903b48660e667c2b6a425d827b7
SCMGuru254 <olivedataetsula@gmail.com>
2025-05-01 23:25:28 +0300

    chore: update capacitor config and package dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,44 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'https',
     cleartext: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#FFFFFF",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      spinnerColor: "#999999",
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "large",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+    StatusBar: {
+      style: 'DARK',
+      backgroundColor: '#FFFFFF'
+    }
+  },
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: true,
+    backgroundColor: "#FFFFFF",
+    iconBackgroundColor: "#FFFFFF",
+    buildOptions: {
+      keystorePath: "android.keystore",
+      keystoreAlias: "key0",
+    }
+  },
+  ios: {
+    contentInset: 'always',
+    allowsLinkPreview: true,
+    scrollEnabled: true,
+    backgroundColor: "#FFFFFF",
+    preferredContentMode: "mobile"
   }
 };
 
